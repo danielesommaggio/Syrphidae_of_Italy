a22bfe224008283014f94e27ed32b3c9a899afbd
Davide Dal Pos <daveliga@gmail.com>
2023-11-30 21:42:24 -0500

    s

---
+++ pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="h-full">
+      <SectionLandscape />
+      <SectionData />
+      <hr class="border-base-muted" />
+      <SectionDescription />
+      <SectionAuthor class="shadow-sm" />
+      <hr class="border-base-muted" />
+      <SectionHighlights />
+      <SectionAnnouncements />
+      <SectionTerms class="drop-shadow" />
+    </div>
+  </template>
+  <script setup>
+  import SectionData from './components/Section/SectionData.vue'
+  import SectionLandscape from './components/Section/SectionLandscape.vue'
+  import SectionDescription from './components/Section/SectionDescription.vue'
+  import SectionTerms from './components/Section/SectionTerms.vue'
+  import SectionHighlights from './components/Section/SectionHighlights.vue'
+  </script>
