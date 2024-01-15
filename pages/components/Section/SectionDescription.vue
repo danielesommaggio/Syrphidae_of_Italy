081d4fe4e63e8e41e3a4c20e71af62606b80de18
Davide Dal Pos <daveliga@gmail.com>
2024-01-15 09:45:18 -0500

    NewUpdateText

--- pages/components/Section/SectionDescription.vue
+++ pages/components/Section/SectionDescription.vue
@@ -1,26 +1,28 @@
 <template>
   <section class="bg-base-foreground">
     <div class="container mx-auto p-4 sm:pt-10 sm:pb-2 relative box-border">
-      <h2 class="px-4 sm:px-0">Introduction</h2>
       <div class="prose !container dark:prose-invert">
         <div>
+          <h2>Introduction</h2>
           <p>
-            The <b>World Ichneumonidae Database (WID)</b> is a comprehensive and 
-            collaborative database for world fauna of Ichneumonidae managed in 
-            <a href="https://taxonworks.org"> TaxonWorks</a>. In this portal 
-            you can find information about distribution, nomenclature, 
-            and biology of the world Darwin wasps.
+            <b>Syrphidae of Italy (SoI)</b> is a comprehensive database 
+            created to include all available information on Italian hoverflies 
+            (Diptera, Syrphidae). In this database, manages in 
+            <a href="https://taxonworks.org"> TaxonWorks</a>, you can find 
+            information about distribution, nomenclature, and biology of 
+            hoverflies species.
           </p>
           <p>
-            The core purpose of the WID is 
-            the research on Ichneumonidae by providing a centralized resource for images, 
-            reference, and taxonomy/nomenclature. Moreover, being updateble and open access,
-            the portal is designed to include all the relevant information updated 
-            constantly.
+            The growing interest in hoverflies, thanks also to the many 
+            ecological services provided such as pollination or pest biological 
+            control, has increased interest and research focused on 
+            these insects in Italy. The purpose of the database is to merge and 
+            make available historical data with new observations.
           </p>
           <p>
-            WID is open to contributions from anyone with specimen records and
-            images.
+            <b>Syrphidae of Italy (SoI)</b> is open to contribution from anyone 
+            with specimen records, images or even just with the desire to get 
+            closer to this fantastic group of insects.
           </p>
         </div>
       </div>
