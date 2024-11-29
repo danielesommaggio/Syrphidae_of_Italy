f639dcac4dce17ed130876a547945389ef153f7f
Davide Dal Pos <daveliga@gmail.com>
2024-11-29 12:59:44 -0500

    Update

--- pages/home.vue
+++ pages/home.vue
@@ -13,8 +13,8 @@
   </template>
   <script setup>
   import SectionData from './components/Section/SectionData.vue'
-  import SectionLandscape from './components/Section/SectionLandscape.vue'
-  import SectionDescription from './components/Section/SectionDescription.vue'
-  import SectionTerms from './components/Section/SectionTerms.vue'
-  import SectionHighlights from './components/Section/SectionHighlights.vue'
+import SectionDescription from './components/Section/SectionDescription.vue'
+import SectionHighlights from './components/Section/SectionHighlights.vue'
+import SectionLandscape from './components/Section/SectionLandscape.vue'
+import SectionTerms from './components/Section/SectionTerms.vue'
   </script>
